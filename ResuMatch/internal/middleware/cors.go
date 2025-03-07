1a552946f767f5588d8f2e0a13c10eb4995e3e9e
Иван <bel02ivan06@mail.ru>
2025-03-07 10:40:25 +0300

    временно переделал структуру вакансии

--- ResuMatch/internal/middleware/cors.go
+++ ResuMatch/internal/middleware/cors.go
@@ -4,9 +4,10 @@ import "net/http"
 
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*") // Разрешить запросы с любого домена
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173") // Разрешить запросы с фронта
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		// Если это preflight-запрос (OPTIONS), просто возвращаем успешный ответ
 		if r.Method == "OPTIONS" {
--- ResuMatch/internal/models/vacancy.go
+++ ResuMatch/internal/models/vacancy.go
@@ -1,17 +1,34 @@
+// package models
+
+// import "time"
+
+// type Vacancy struct {
+// 	ID          int       `json:"id"`
+// 	Title       string    `json:"title"`
+// 	Company     string    `json:"company"`
+// 	Location    string    `json:"location"`
+// 	Salary      string    `json:"salary"`
+// 	Description string    `json:"description"`
+// 	CreatedAt   time.Time `json:"created_at"`
+// 	UpdatedAt   time.Time `json:"updated_at"`
+// 	Active      bool      `json:"active"`
+// 	PostedBy    int       `json:"posted_by"`   // ID пользователя, разместившего вакансию
+// 	EmployerID  int       `json:"employer_id"` // ID компании-работодателя
+// }
+
 package models
 
-import "time"
-
 type Vacancy struct {
-	ID          int       `json:"id"`
-	Title       string    `json:"title"`
-	Company     string    `json:"company"`
-	Location    string    `json:"location"`
-	Salary      string    `json:"salary"`
-	Description string    `json:"description"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
-	Active      bool      `json:"active"`
-	PostedBy    int       `json:"posted_by"`   // ID пользователя, разместившего вакансию
-	EmployerID  int       `json:"employer_id"` // ID компании-работодателя
+	ID         int     `json:"id"`
+	Profession string  `json:"profession"`
+	Salary     string  `json:"salary"`
+	Company    string  `json:"company"`
+	City       string  `json:"city"`
+	Badges     []Badge `json:"badges"`
+	DayCreated int     `json:"day_created"`
+	Count      int     `json:"count"`
+}
+
+type Badge struct {
+	Name string `json:"name"`
 }

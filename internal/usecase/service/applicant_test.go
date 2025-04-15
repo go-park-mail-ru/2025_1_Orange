2ecd4d218dcda71a0a389ddd8486d00728ebbde7
vcreatorv <valery.nagapetyan@yandex.ru>
2025-04-15 18:00:07 +0300

    Added other for updating applicant profile

--- internal/usecase/service/applicant_test.go
+++ internal/usecase/service/applicant_test.go
@@ -11,6 +11,7 @@ import (
 	"go.uber.org/mock/gomock"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestApplicantService_Register(t *testing.T) {
@@ -321,6 +322,449 @@ func TestApplicantService_Login(t *testing.T) {
 				require.Error(t, err)
 				var serviceErr entity.Error
 				require.ErrorAs(t, err, &serviceErr)
+				require.Equal(t, tc.expectedErr.Error(), err.Error())
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestApplicantService_UpdateProfile(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name        string
+		userID      int
+		input       *dto.ApplicantProfileUpdate
+		mockSetup   func(*mock.MockApplicantRepository, *mock.MockCityRepository)
+		expectedErr error
+	}{
+		{
+			name:   "Успешное обновление всех полей",
+			userID: 1,
+			input: &dto.ApplicantProfileUpdate{
+				FirstName:  "НовоеИмя",
+				LastName:   "НоваяФамилия",
+				MiddleName: "НовоеОтчество",
+				BirthDate:  time.Date(1990, 4, 2, 0, 0, 0, 0, time.UTC),
+				Sex:        "M",
+				Status:     "actively_searching",
+				Quote:      "Один за всех и все за одного",
+				Vk:         "https://vk.com/updated_profile",
+				Telegram:   "https://t.me/updated_profile",
+				Facebook:   "https://facebook.com/updated_profile",
+				City:       "Москва",
+			},
+			mockSetup: func(applicantRepo *mock.MockApplicantRepository, cityRepo *mock.MockCityRepository) {
+				cityRepo.EXPECT().
+					GetCityByName(gomock.Any(), "Москва").
+					Return(&entity.City{ID: 1}, nil)
+
+				applicantRepo.EXPECT().
+					UpdateApplicant(gomock.Any(), 1, map[string]interface{}{
+						"first_name":  "НовоеИмя",
+						"last_name":   "НоваяФамилия",
+						"middle_name": "НовоеОтчество",
+						"birth_date":  time.Date(1990, 4, 2, 0, 0, 0, 0, time.UTC),
+						"sex":         "M",
+						"status":      "actively_searching",
+						"quote":       "Один за всех и все за одного",
+						"vk":          "https://vk.com/updated_profile",
+						"telegram":    "https://t.me/updated_profile",
+						"facebook":    "https://facebook.com/updated_profile",
+						"city_id":     1,
+					}).
+					Return(nil)
+			},
+			expectedErr: nil,
+		},
+		{
+			name:   "Успешное обновление города",
+			userID: 1,
+			input: &dto.ApplicantProfileUpdate{
+				City: "Москва",
+			},
+			mockSetup: func(applicantRepo *mock.MockApplicantRepository, cityRepo *mock.MockCityRepository) {
+				cityRepo.EXPECT().
+					GetCityByName(gomock.Any(), "Москва").
+					Return(&entity.City{ID: 1}, nil)
+				applicantRepo.EXPECT().
+					UpdateApplicant(gomock.Any(), 1, map[string]interface{}{
+						"city_id": 1,
+					}).
+					Return(nil)
+			},
+			expectedErr: nil,
+		},
+		{
+			name:   "Невалидное имя (слишком длинное)",
+			userID: 1,
+			input: &dto.ApplicantProfileUpdate{
+				FirstName: strings.Repeat("a", 31),
+			},
+			mockSetup: func(*mock.MockApplicantRepository, *mock.MockCityRepository) {},
+			expectedErr: entity.NewError(
+				entity.ErrBadRequest,
+				fmt.Errorf("имя не может быть длиннее 30 символов"),
+			),
+		},
+		{
+			name:   "Невалидная фамилия (слишком длинная)",
+			userID: 1,
+			input: &dto.ApplicantProfileUpdate{
+				LastName: strings.Repeat("a", 31),
+			},
+			mockSetup: func(*mock.MockApplicantRepository, *mock.MockCityRepository) {},
+			expectedErr: entity.NewError(
+				entity.ErrBadRequest,
+				fmt.Errorf("фамилия не может быть длиннее 30 символов"),
+			),
+		},
+		{
+			name:   "Невалидное отчество (слишком длинное)",
+			userID: 1,
+			input: &dto.ApplicantProfileUpdate{
+				MiddleName: strings.Repeat("a", 31),
+			},
+			mockSetup: func(*mock.MockApplicantRepository, *mock.MockCityRepository) {},
+			expectedErr: entity.NewError(
+				entity.ErrBadRequest,
+				fmt.Errorf("отчество не может быть длиннее 30 символов"),
+			),
+		},
+		{
+			name:   "Невалидная цитата (слишком длинная)",
+			userID: 1,
+			input: &dto.ApplicantProfileUpdate{
+				Quote: strings.Repeat("a", 256),
+			},
+			mockSetup: func(*mock.MockApplicantRepository, *mock.MockCityRepository) {},
+			expectedErr: entity.NewError(
+				entity.ErrBadRequest,
+				fmt.Errorf("цитата не может быть длиннее 255 символов"),
+			),
+		},
+		{
+			name:   "Невалидная ссылка ВК",
+			userID: 1,
+			input: &dto.ApplicantProfileUpdate{
+				Vk: strings.Repeat("a", 129),
+			},
+			mockSetup: func(*mock.MockApplicantRepository, *mock.MockCityRepository) {},
+			expectedErr: entity.NewError(
+				entity.ErrBadRequest,
+				fmt.Errorf("url не может быть длиннее 128 символов"),
+			),
+		},
+		{
+			name:   "Невалидная ссылка Телеграм",
+			userID: 1,
+			input: &dto.ApplicantProfileUpdate{
+				Telegram: strings.Repeat("a", 129),
+			},
+			mockSetup: func(*mock.MockApplicantRepository, *mock.MockCityRepository) {},
+			expectedErr: entity.NewError(
+				entity.ErrBadRequest,
+				fmt.Errorf("url не может быть длиннее 128 символов"),
+			),
+		},
+		{
+			name:   "Невалидная ссылка facebook",
+			userID: 1,
+			input: &dto.ApplicantProfileUpdate{
+				Facebook: strings.Repeat("a", 129),
+			},
+			mockSetup: func(*mock.MockApplicantRepository, *mock.MockCityRepository) {},
+			expectedErr: entity.NewError(
+				entity.ErrBadRequest,
+				fmt.Errorf("url не может быть длиннее 128 символов"),
+			),
+		},
+		{
+			name:   "Невалидный пол",
+			userID: 1,
+			input: &dto.ApplicantProfileUpdate{
+				Sex: "X",
+			},
+			mockSetup: func(*mock.MockApplicantRepository, *mock.MockCityRepository) {},
+			expectedErr: entity.NewError(
+				entity.ErrBadRequest,
+				fmt.Errorf("пол должен быть 'M' или 'F'"),
+			),
+		},
+		{
+			name:   "Невалидная дата рождения",
+			userID: 1,
+			input: &dto.ApplicantProfileUpdate{
+				BirthDate: time.Now().Add(24 * time.Hour),
+			},
+			mockSetup: func(*mock.MockApplicantRepository, *mock.MockCityRepository) {},
+			expectedErr: entity.NewError(
+				entity.ErrBadRequest,
+				fmt.Errorf("дата рождения не может быть позже текущей даты"),
+			),
+		},
+		{
+			name:   "Невалидный статус",
+			userID: 1,
+			input: &dto.ApplicantProfileUpdate{
+				Status: "InvalidStatus",
+			},
+			mockSetup: func(*mock.MockApplicantRepository, *mock.MockCityRepository) {},
+			expectedErr: entity.NewError(
+				entity.ErrBadRequest,
+				fmt.Errorf("неверный статус соискателя"),
+			),
+		},
+		{
+			name:   "Город не найден",
+			userID: 1,
+			input: &dto.ApplicantProfileUpdate{
+				City: "НесуществующийГород",
+			},
+			mockSetup: func(applicantRepo *mock.MockApplicantRepository, cityRepo *mock.MockCityRepository) {
+				cityRepo.EXPECT().
+					GetCityByName(gomock.Any(), "НесуществующийГород").
+					Return(nil, entity.NewError(
+						entity.ErrNotFound,
+						fmt.Errorf("город с name=НесуществующийГород не найден"),
+					),
+					)
+			},
+			expectedErr: entity.NewError(
+				entity.ErrNotFound,
+				fmt.Errorf("город с name=НесуществующийГород не найден"),
+			),
+		},
+		{
+			name:   "Ошибка при обновлении",
+			userID: 1,
+			input: &dto.ApplicantProfileUpdate{
+				FirstName: "Имя",
+			},
+			mockSetup: func(applicantRepo *mock.MockApplicantRepository, cityRepo *mock.MockCityRepository) {
+				applicantRepo.EXPECT().
+					UpdateApplicant(gomock.Any(), 1, map[string]interface{}{
+						"first_name": "Имя",
+					}).
+					Return(entity.NewError(
+						entity.ErrInternal,
+						fmt.Errorf("не удалось обновить соискателя с id=1"),
+					),
+					)
+			},
+			expectedErr: entity.NewError(
+				entity.ErrInternal,
+				fmt.Errorf("не удалось обновить соискателя с id=1"),
+			),
+		},
+		{
+			name:      "Нет полей для обновления",
+			userID:    1,
+			input:     &dto.ApplicantProfileUpdate{},
+			mockSetup: func(*mock.MockApplicantRepository, *mock.MockCityRepository) {},
+			expectedErr: entity.NewError(
+				entity.ErrBadRequest,
+				fmt.Errorf("отсутствуют поля для обновления"),
+			),
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockApplicantRepo := mock.NewMockApplicantRepository(ctrl)
+			mockCityRepo := mock.NewMockCityRepository(ctrl)
+			applicantService := NewApplicantService(mockApplicantRepo, mockCityRepo, nil)
+
+			tc.mockSetup(mockApplicantRepo, mockCityRepo)
+
+			err := applicantService.UpdateProfile(context.Background(), tc.userID, tc.input)
+
+			if tc.expectedErr != nil {
+				require.Error(t, err)
+				var serviceErr entity.Error
+				require.ErrorAs(t, err, &serviceErr)
+				require.Equal(t, tc.expectedErr.Error(), err.Error())
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestApplicantService_GetUser(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name           string
+		applicantID    int
+		mockSetup      func(*mock.MockApplicantRepository, *mock.MockCityRepository)
+		expectedResult *dto.ApplicantProfileResponse
+		expectedErr    error
+	}{
+		{
+			name:        "Успешное получение профиля",
+			applicantID: 1,
+			mockSetup: func(appRepo *mock.MockApplicantRepository, cityRepo *mock.MockCityRepository) {
+				appRepo.EXPECT().
+					GetApplicantByID(gomock.Any(), 1).
+					Return(&entity.Applicant{
+						ID:         1,
+						FirstName:  "Иван",
+						LastName:   "Иванов",
+						MiddleName: "Иванович",
+						BirthDate:  time.Date(1990, 1, 1, 0, 0, 0, 0, time.UTC),
+						Sex:        "M",
+						Status:     "actively_searching",
+						Quote:      "Тестовая цитата",
+						Vk:         "https://vk.com/ivanov",
+						Telegram:   "https://t.me/ivanov",
+						Facebook:   "https://facebook.com/ivanov",
+						CityID:     1,
+					}, nil)
+
+				cityRepo.EXPECT().
+					GetCityByID(gomock.Any(), 1).
+					Return(&entity.City{ID: 1, Name: "Москва"}, nil)
+			},
+			expectedResult: &dto.ApplicantProfileResponse{
+				ID:         1,
+				FirstName:  "Иван",
+				LastName:   "Иванов",
+				MiddleName: "Иванович",
+				BirthDate:  time.Date(1990, 1, 1, 0, 0, 0, 0, time.UTC),
+				Sex:        "M",
+				Status:     "actively_searching",
+				Quote:      "Тестовая цитата",
+				Vk:         "https://vk.com/ivanov",
+				Telegram:   "https://t.me/ivanov",
+				Facebook:   "https://facebook.com/ivanov",
+				City:       "Москва",
+			},
+			expectedErr: nil,
+		},
+		{
+			name:        "Пользователь не найден",
+			applicantID: 999,
+			mockSetup: func(appRepo *mock.MockApplicantRepository, cityRepo *mock.MockCityRepository) {
+				appRepo.EXPECT().
+					GetApplicantByID(gomock.Any(), 999).
+					Return(nil,
+						entity.NewError(
+							entity.ErrNotFound,
+							fmt.Errorf("соискатель с id=999 не найден"),
+						),
+					)
+			},
+			expectedResult: nil,
+			expectedErr: entity.NewError(
+				entity.ErrNotFound,
+				fmt.Errorf("соискатель с id=999 не найден"),
+			),
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockAppRepo := mock.NewMockApplicantRepository(ctrl)
+			mockCityRepo := mock.NewMockCityRepository(ctrl)
+			applicantService := NewApplicantService(mockAppRepo, mockCityRepo, nil)
+
+			tc.mockSetup(mockAppRepo, mockCityRepo)
+
+			result, err := applicantService.GetUser(context.Background(), tc.applicantID)
+
+			if tc.expectedErr != nil {
+				require.Error(t, err)
+				var serviceErr entity.Error
+				require.ErrorAs(t, err, &serviceErr)
+				require.Equal(t, tc.expectedErr.Error(), err.Error())
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tc.expectedResult, result)
+			}
+		})
+	}
+}
+
+func TestApplicantService_UpdateAvatar(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name        string
+		userID      int
+		avatarID    int
+		mockSetup   func(*mock.MockApplicantRepository)
+		expectedErr error
+	}{
+		{
+			name:     "Успешное обновление аватара",
+			userID:   1,
+			avatarID: 100,
+			mockSetup: func(appRepo *mock.MockApplicantRepository) {
+				appRepo.EXPECT().
+					UpdateApplicant(
+						gomock.Any(),
+						1,
+						map[string]interface{}{"avatar_id": 100},
+					).
+					Return(nil)
+			},
+			expectedErr: nil,
+		},
+		{
+			name:     "Ошибка обновления avatarID",
+			userID:   2,
+			avatarID: 1,
+			mockSetup: func(appRepo *mock.MockApplicantRepository) {
+				appRepo.EXPECT().
+					UpdateApplicant(
+						gomock.Any(),
+						2,
+						map[string]interface{}{"avatar_id": 1},
+					).
+					Return(entity.NewError(
+						entity.ErrInternal,
+						fmt.Errorf("не удалось обновить соискателя с id=1"),
+					))
+			},
+			expectedErr: entity.NewError(
+				entity.ErrInternal,
+				fmt.Errorf("не удалось обновить соискателя с id=1"),
+			),
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockAppRepo := mock.NewMockApplicantRepository(ctrl)
+			service := NewApplicantService(mockAppRepo, nil, nil)
+
+			tc.mockSetup(mockAppRepo)
+
+			err := service.UpdateAvatar(context.Background(), tc.userID, tc.avatarID)
+
+			if tc.expectedErr != nil {
+				require.Error(t, err)
 				require.Equal(t, tc.expectedErr.Error(), err.Error())
 			} else {
 				require.NoError(t, err)

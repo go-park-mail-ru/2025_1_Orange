c936ee884742c2b70afe38f5faa4a36a69f7f874
samarskiyad <samarskiyad@student.bmstu.ru>
2025-05-07 16:51:50 +0300

    Change metrics tests

--- internal/middleware/metrics_test.go
+++ internal/middleware/metrics_test.go
@@ -8,28 +8,22 @@ import (
 
 	"ResuMatch/internal/metrics"
 
-	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stretchr/testify/require"
 )
 
 func TestMetricsMiddleware(t *testing.T) {
 	t.Parallel()
 
-	metrics := &metrics.Metrics{
-		RequestCounter:  prometheus.NewCounterVec(prometheus.CounterOpts{Name: "requests_total"}, []string{"method", "path", "status"}),
-		RequestDuration: prometheus.NewHistogramVec(prometheus.HistogramOpts{Name: "request_duration_seconds"}, []string{"method", "path"}),
-		ResponseSize:    prometheus.NewHistogramVec(prometheus.HistogramOpts{Name: "response_size_bytes"}, []string{"method", "path"}),
-		ErrorCounter:    prometheus.NewCounterVec(prometheus.CounterOpts{Name: "errors_total"}, []string{"method", "path", "status"}),
-	}
-
-	prometheus.MustRegister(metrics.RequestCounter, metrics.RequestDuration, metrics.ResponseSize, metrics.ErrorCounter)
+	// Инициализация метрик
+	metrics.Init("test_namespace")
 
 	t.Run("Successful request", func(t *testing.T) {
 		t.Parallel()
-		w := httptest.NewRecorder()
-		r := httptest.NewRequest("GET", "/test", nil)
 
-		handler := MetricsMiddleware(metrics)(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest("GET", "/test/path", nil)
+
+		handler := MetricsMiddleware()(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusOK)
 			_, err := w.Write([]byte("Hello, world!"))
 			require.NoError(t, err)
@@ -40,26 +34,33 @@ func TestMetricsMiddleware(t *testing.T) {
 		//duration := time.Since(start).Seconds()
 
 		require.Equal(t, http.StatusOK, w.Code)
-		require.Equal(t, "Hello, world!", w.Body.String())
 
-		require.NotNil(t, metrics.RequestCounter.WithLabelValues("GET", "/test", strconv.Itoa(http.StatusOK)))
-		require.NotNil(t, metrics.RequestDuration.WithLabelValues("GET", "/test"))
-		require.NotNil(t, metrics.ResponseSize.WithLabelValues("GET", "/test"))
+		normalizedPath := metrics.NormalizePath("/test/path")
+		status := strconv.Itoa(http.StatusOK)
+
+		require.NotNil(t, metrics.RequestCounter.WithLabelValues("GET", normalizedPath, status))
+		require.NotNil(t, metrics.RequestDuration.WithLabelValues("GET", normalizedPath))
+		require.NotNil(t, metrics.ResponseSize.WithLabelValues("GET", normalizedPath))
 	})
 
 	t.Run("Error response", func(t *testing.T) {
 		t.Parallel()
-		w := httptest.NewRecorder()
-		r := httptest.NewRequest("POST", "/fail", nil)
 
-		handler := MetricsMiddleware(metrics)(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest("POST", "/error/path", nil)
+
+		handler := MetricsMiddleware()(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "error", http.StatusInternalServerError)
 		}))
 
 		handler.ServeHTTP(w, r)
 
 		require.Equal(t, http.StatusInternalServerError, w.Code)
-		require.NotNil(t, metrics.ErrorCounter.WithLabelValues("POST", "/fail", strconv.Itoa(http.StatusInternalServerError)))
+
+		normalizedPath := metrics.NormalizePath("/error/path")
+		status := strconv.Itoa(http.StatusInternalServerError)
+
+		require.NotNil(t, metrics.ErrorCounter.WithLabelValues("POST", normalizedPath, status))
 	})
 }
 
